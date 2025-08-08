e4f98ef01132540f5d0d79e709ac11aaf703e87d
esther olubiyi <olubiyisther@gmail.com>
2025-08-08 17:07:25 +0100

    updated

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div className="justify-center items-center text-2xl bg-[#f8f8f8] py-30">
+    <div className="justify-center items-center text-2xl bg-[#f8f8f8] py-30 h-screen">
       <p className="text-center text-black font-bold text-5xl">Coming soon</p>
     </div>
   )
